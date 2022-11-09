314273ff744a6a71a810ec4f7e8e119c640557c0
Quang Huy <ltqhuy.th0112@gmail.com>
2022-11-09 16:13:44 +0700

    fix Ui

--- my-app/src/Components/ProductList.jsx
+++ my-app/src/Components/ProductList.jsx
@@ -8,10 +8,20 @@ export default function ProductList(props) {
             <div className="card">
                 <img className="card-img-top" src={item.image} alt="Title" />
                 <div className="card-body bg-danger py-4 ">
+                    <h4 className="card-title pb-2">{item.name}</h4>
                     <ModalProduct item={item} />
                 </div>
             </div>
+            {/* <div className="card">
+                <img className="card-img-top" src="holder.js/100x180/" alt="Title" />
+                <div className="card-body">
+                    <h4 className="card-title">Title</h4>
+                    <p className="card-text">Text</p>
+                </div>
+            </div> */}
+
         </>
+
 
     )
 }
